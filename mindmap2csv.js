2555b574a23ab90b839b8136413984ccd7aa8beb
Zihao Zhu <zihao@azzh.me>
2022-02-16 20:37:33 +0800

    logic updates

--- mindmap2csv.js
+++ mindmap2csv.js
@@ -3,7 +3,6 @@
 document.addEventListener("click", fileTransform);
 
 /**
- * TODO: rename this function
  * @param {Node} parentNode
  */
 function buildCsvFromRootNode(node, csvDocument) {
@@ -12,7 +11,11 @@ function buildCsvFromRootNode(node, csvDocument) {
   // Then loop
 
   // Bottom of NodeList
-  if (!node.children.length) return;
+  // TODO: enum possible error types?
+  // ^ is it still necessary if accomplished in function before that
+
+  // TODO: figure out how to get object length or how to descend
+  if (!node.children.length) return 0;
 
   // Add new row
 
@@ -38,8 +41,15 @@ function buildCsvFromRootNode(node, csvDocument) {
 function fileTransform() {
   const xmlDocument = parseInputToXml();
   const csvDocument = convertXmlToCsv(xmlDocument);
-  console.log(csvDocument);
-  //   doAutoDownloadCsv();
+
+  // TODO: list enum possible response types
+  // no nodes, no content, does not start with <outline> etc
+  if (csvDocument !== 0) {
+    console.log(csvDocument);
+    // doAutoDownloadCsv();
+  } else {
+    window.alert("Invalid document content. Nothing parsed.");
+  }
 }
 
 /**
@@ -58,11 +68,11 @@ function parseInputToXml() {
     <ownerName></ownerName>
   </head>
   <body>
-<outline text="Top level">
-  <outline text="child L1">
+<outline text="Top level root node">
+  <outline text="child L1 - 010">
   </outline>
-  <outline text="child L1">
-    <outline text="child L2">
+  <outline text="child L1 - 020">
+    <outline text="child L2 - 021 ">
     </outline>
   </outline>
 </outline>
@@ -78,22 +88,39 @@ function parseInputToXml() {
 /**
  * Convert parsed XMLDocument to a CSV array then trigger auto download
  *
- * @param {XML} xmlDocument
+ * @param {XMLDocument} xmlDocument
  */
 function convertXmlToCsv(xmlDocument) {
   let csvHeader = "data:text/csv;charset=utf-8,";
 
   let maxWidth = 0,
     col = 0,
-    csvDocument;
+    csvDocument,
+    documentRootNode;
 
-  const documentRootNode = xmlDocument.getElementsByTagName("body")[0];
-
-  if (!documentRootNode.children.length) {
-    let csvData = buildCsvFromRootNode(documentRootNode);
-    csvDocument = csvHeader + csvData.map((e) => e.join(",")).join("\n");
+  // This assumes opening tag inside <body> to be <outline>
+  // A valid assumption since we are supposed to work with OPML
+  // This reduces the computation effort as compared to parsing <body> first
+  if (xmlDocument.getElementsByTagName("outline")[0]) {
+    documentRootNode = xmlDocument.getElementsByTagName("outline")[0];
   } else {
-    // sth bad happens jk
+    return 0;
+  }
+
+  if (documentRootNode.children.length) {
+    // TODO: cast documentRootNode.children from HTMLCollection to object
+    console.log(documentRootNode.children);
+
+    let csvData = buildCsvFromRootNode(documentRootNode);
+
+    if (csvData !== 0) {
+      csvDocument = csvHeader + csvData.map((e) => e.join(",")).join("\n");
+    } else {
+      console.log("csvDocument is not mapped");
+    }
+  } else {
+    // TODO: enum possible outcomes
+    csvDocument = "This document has no content";
   }
 
   // Align last 2 non-empty cells in each row to same column
